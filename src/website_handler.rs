290b273ddc7dd0bd2d60942c17aa7b9b25f421f9
Eric McKevitt <ericmckevitt@Erics-MacBook-Pro-2.local>
2023-01-10 20:44:52 -0700

    handle_request() now matches on method types, only with support for GET so far

--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,10 +1,18 @@
 use super::server::Handler;
-use super::http::{Request, Response, StatusCode};
+use super::http::{Request, Response, StatusCode, Method};
 
 pub struct WebsiteHandler;
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
-        Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                // If method is GET, specify all paths we want to handle
+                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())), 
+                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello!</h1>".to_string())), 
+                _  => Response::new(StatusCode::NotFound, None),
+            },
+            _  => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
