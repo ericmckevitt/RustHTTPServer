686255bcd680d972957a318f0cc5cd9432f7b45b
Eric McKevitt <ericmckevitt@Erics-MacBook-Pro-2.local>
2023-01-11 17:31:41 -0700

    Added public directory, server now can serve any files with directory traversal attack protection

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@
 
 use server::Server;
 use website_handler::WebsiteHandler;
+use std::env;
 
 mod http;
 mod server;
 mod website_handler;
 
 fn main() {
+    // PUBLIC_PATH=$(pwd)/public cargo run   
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public path: {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);
+    server.run(WebsiteHandler::new(public_path));
 }
--- src/website_handler.rs
+++ src/website_handler.rs
@@ -1,16 +1,49 @@
 use super::server::Handler;
 use super::http::{Request, Response, StatusCode, Method};
+use std::fs::{self, canonicalize};
 
-pub struct WebsiteHandler;
+pub struct WebsiteHandler {
+    public_path: String
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self {
+            public_path
+        }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        // Append file_path to public_path
+        let path = format!("{}/{}", self.public_path, file_path);
+
+        // Check canonical path to see if it is part of public directory 
+        match fs::canonicalize(path) {
+            Ok(path) => { 
+                if path.starts_with(&self.public_path) {
+                    // .ok() converts Result<T, E> to Option<T> and discards any errors
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack Detected: {}", file_path);
+                    None
+                }
+            }, 
+            Err(_) => None,
+        }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response {
         match request.method() {
             Method::GET => match request.path() {
                 // If method is GET, specify all paths we want to handle
-                "/" => Response::new(StatusCode::Ok, Some("<h1>Welcome</h1>".to_string())), 
-                "/hello" => Response::new(StatusCode::Ok, Some("<h1>Hello!</h1>".to_string())), 
-                _  => Response::new(StatusCode::NotFound, None),
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html")), 
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html")), 
+                path  => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                }
             },
             _  => Response::new(StatusCode::NotFound, None),
         }
